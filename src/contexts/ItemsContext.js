21288a2884485c17f0e397466c9143d5c8ffc540
Justin Spegele <justin.spegele@gmail.com>
2024-07-11 15:56:18 -0400

    Created a custom hook for useLocalStorage to simplify ItemsContext

--- src/contexts/ItemsContext.js
+++ src/contexts/ItemsContext.js
@@ -1,48 +1,26 @@
-import React, { useState, createContext } from "react"
+import React, { useState, useEffect, createContext, useCallback } from "react"
 import { getDatabase, ref, get, set, push } from "firebase/database"
 import { DateTime } from "luxon"
+import { useLocalStorage } from "../hooks/useLocalStorage"
 
 export const ItemsContext = createContext()
 
 const initialState = []
 
-const saveToLocalStorage = (items) => {
-  const database = getDatabase()
-
-  get(ref(database, "itemsVersion"))
-    .then((snap) => {
-      localStorage.setItem("checklistItems", JSON.stringify(items))
-      localStorage.setItem("checklistItemsVersion", JSON.stringify(snap.val()))
-    })
-    .catch((error) => {
-      console.error(error)
-    })
-}
-
-const loadItemsFromLocalStorage = () => {
-  return new Promise((resolve, reject) => {
-    if (localStorage.getItem("checklistItems") === null) reject("Local data not found")
-
-    resolve(JSON.parse(localStorage.getItem("checklistItems")))
-  })
-}
-
 const loadItemsFromDatabase = () => {
   const database = getDatabase()
-
   return new Promise((resolve, reject) => {
     get(ref(database, "items/"))
       .then((snap) => {
         const dataArray = []
         if (snap.exists()) {
           for (const [key, value] of Object.entries(snap.val())) {
-            dataArray.push({
-              id: key,
-              ...value,
-            })
+            dataArray.push({ id: key, ...value })
           }
         }
-        const sortedDataArray = dataArray.sort((a, b) => a.name > b.name)
+        const sortedDataArray = dataArray.sort((a, b) =>
+          a.name > b.name ? 1 : -1
+        )
         resolve(sortedDataArray)
       })
       .catch((error) => {
@@ -53,64 +31,39 @@ const loadItemsFromDatabase = () => {
 
 export const ItemsProvider = (props) => {
   const database = getDatabase()
-
   const [itemsState, setItemsState] = useState(initialState)
+  const [localItems, setLocalItems] = useLocalStorage(
+    "checklistItems",
+    initialState
+  )
+  const [localVersion, setLocalVersion] = useLocalStorage(
+    "checklistItemsVersion",
+    null
+  )
 
   const clearItemsState = () => setItemsState(initialState)
 
-  const startSetItems = () => {
-    return new Promise((resolve, reject) => {
-      // Compare versions, pull from db if local version is out of date
-      const localDateText = localStorage.getItem("checklistItemsVersion") ? JSON.parse(localStorage.getItem("checklistItemsVersion")) : null
-      const localDataSet = localStorage.getItem("checklistItems") ? true : false
+  const startSetItems = useCallback(async () => {
+    try {
+      const serverVersionSnap = await get(ref(database, "itemsVersion"))
+      const serverVersion = serverVersionSnap.val()
 
-      // Local data not set, load from server
-      if (!localDateText || !localDataSet) {
-        loadItemsFromDatabase()
-          .then((data) => {
-            setItemsState(data)
-            saveToLocalStorage(data)
-            resolve("success")
-          })
-          .catch((error) => {
-            console.error(error)
-            reject(error)
-          })
+      if (localVersion !== serverVersion) {
+        const data = await loadItemsFromDatabase()
+        setItemsState(data)
+        setLocalItems(data)
+        setLocalVersion(serverVersion)
+      } else {
+        setItemsState(localItems)
       }
-      
-      get(ref(database, "itemsVersion"))
-        .then((snap) => {
-          const serverDateText = snap.val()
+    } catch (error) {
+      console.error("Error setting items", error)
+    }
+  }, [database, localItems, localVersion, setLocalItems, setLocalVersion])
 
-          // if local version is equal, load items from localStorage
-          if (localDateText === serverDateText) {
-            loadItemsFromLocalStorage()
-              .then((data) => {
-                setItemsState(data)
-                resolve("success")
-              })
-              .catch((error) => {
-                console.error(error)
-                reject(error)
-              })
-          } else {
-            loadItemsFromDatabase()
-              .then((data) => {
-                setItemsState(data)
-                saveToLocalStorage(data)
-                resolve("success")
-              })
-              .catch((error) => {
-                console.error(error)
-                reject(error)
-              })
-          }
-        })
-        .catch((error) => {
-          console.error(error)
-        })
-    })
-  }
+  useEffect(() => {
+    startSetItems()
+  }, [startSetItems])
 
   const setItem = (item, id) => {
     setItemsState((prevState) => [
@@ -120,10 +73,8 @@ export const ItemsProvider = (props) => {
   }
 
   const startSetItem = (item) => {
-    // set version date
     set(ref(database, "itemsVersion"), DateTime.now().toISO())
 
-    // set item
     const { id, ...restOfItem } = item
     return new Promise((resolve, reject) => {
       if (id) {
@@ -132,14 +83,14 @@ export const ItemsProvider = (props) => {
             setItem(restOfItem, id)
             resolve("success")
           })
-          .catch((error) => reject(error))
+          .catch(reject)
       } else {
         push(ref(database, "items/"), restOfItem)
           .then((response) => {
             setItem(restOfItem, response.key)
             resolve("success")
           })
-          .catch((error) => reject(error))
+          .catch(reject)
       }
     })
   }
